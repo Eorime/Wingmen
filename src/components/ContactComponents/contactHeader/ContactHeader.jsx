36492ec8f94959f03f4be5d814ccece15b652636
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-05-20 13:04:17 +0400

    Create contact wireframe

---
+++ src/components/ContactComponents/contactHeader/ContactHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./style";
+
+const ContactHeader = () => {
+  return <Container>CONTACT</Container>;
+};
+
+export default ContactHeader;
---
+++ src/components/ContactComponents/contactHeader/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: absolute;
+  top: 200px;
+  left: 120px;
+  font-size: 48px;
+`;
