e075cf74430c3480653cb29baa0c9c869247da3f
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-06-06 16:16:13 +0400

    Create the new project page

--- src/pages/AdminAddPost/AdminAddPost.jsx
+++ src/pages/AdminAddPost/AdminAddPost.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
-import { Container } from "./style";
+import {
+  Button,
+  Container,
+  ErrorMessage,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "./style";
 import AdminNavigation from "../../components/adminComponents/adminNavigation/AdminNavigation";
+import { postData } from "../../api";
 
 const AdminAddPost = () => {
   const [formData, setFormData] = useState({
@@ -11,9 +20,66 @@ const AdminAddPost = () => {
   const [error, setError] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
 
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await postData(
+        "http://localhost:5000/projects/newProject",
+        formData
+      );
+      setSuccessMessage("Project added successfully");
+
+      setFormData({ name: "", date: "", description: "" });
+    } catch (error) {
+      setError("Couldn't create project");
+    }
+  };
+
   return (
     <Container>
       <AdminNavigation />
+      <Form onSubmit={handleChange}>
+        <FormGroup>
+          <Label>Project Name:</Label>
+          <Input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label>Date:</Label>
+          <Input
+            type="text"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label>Description:</Label>
+          <Input
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
+        </FormGroup>
+        <Button type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      {successMessage && <p>{successMessage}</p>}
     </Container>
   );
 };
--- src/pages/AdminAddPost/style.js
+++ src/pages/AdminAddPost/style.js
@@ -1,3 +1,18 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin-left: 50%;
+  margin-top: 10%;
+`;
+
+export const Form = styled.form``;
+
+export const FormGroup = styled.div``;
+
+export const Label = styled.label``;
+
+export const Input = styled.input``;
+
+export const Button = styled.button``;
+
+export const ErrorMessage = styled.p``;
