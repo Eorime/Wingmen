a005ea623f8328214c3423d509ece14e9c83da87
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-06-10 17:14:13 +0400

    Add inquiry sending functionality

--- src/components/LandingComponents/inquiryForm/InquiryForm.jsx
+++ src/components/LandingComponents/inquiryForm/InquiryForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   EmailInput,
@@ -8,20 +8,52 @@ import {
   Label,
   SendButton,
 } from "./style";
+import { postData } from "../../../api";
 
 const InquiryForm = () => {
+  const [email, setEmail] = useState("");
+  const [inquiry, setInquiry] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSend = async () => {
+    try {
+      await postData("http://localhost:5000/inquiry", {
+        email: email,
+        inquiry: inquiry,
+      });
+
+      setEmail("");
+      setInquiry("");
+    } catch (error) {
+      setError("Couldn't send inquiry");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSend();
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <FormGroup>
           <Label>Email</Label>
-          <EmailInput type="text"></EmailInput>
+          <EmailInput
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></EmailInput>
         </FormGroup>
         <FormGroup>
           <Label>Inquiry</Label>
-          <InquiryInput type="text"></InquiryInput>
+          <InquiryInput
+            type="text"
+            value={inquiry}
+            onChange={(e) => setInquiry(e.target.value)}
+          ></InquiryInput>
         </FormGroup>
-        <SendButton>SEND</SendButton>
+        <SendButton type="submit">SEND</SendButton>
       </Form>
     </Container>
   );
