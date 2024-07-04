9621363d70bb41fc916ed00350b060e40cb4deb9
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-07-04 16:35:50 +0400

    Add description on the home page

--- src/components/LandingComponents/description/Description.jsx
+++ src/components/LandingComponents/description/Description.jsx
@@ -1,8 +1,26 @@
 import React from "react";
-import { Container } from "./style";
+import {
+  Container,
+  DescriptionContainer,
+  DescriptionHeader,
+  DescriptionSubHeader,
+  DescriptionText,
+} from "./style";
 
 const Description = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <DescriptionContainer>
+        <DescriptionHeader>WINGMEN</DescriptionHeader>
+        <DescriptionSubHeader>/ˈwɪŋmən/</DescriptionSubHeader>
+        <DescriptionText>
+          in the skies of entrepreneurship full of ups and downs, [wingman],
+          represents your helper who supports from behind, below, above, and to
+          the side.
+        </DescriptionText>
+      </DescriptionContainer>
+    </Container>
+  );
 };
 
 export default Description;
--- src/components/LandingComponents/description/style.js
+++ src/components/LandingComponents/description/style.js
@@ -2,10 +2,32 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
-export const DescriptionContainer = styled.div``;
+export const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 140px;
+  margin-left: 75px;
+`;
 
-export const DescriptionHeader = styled.h1``;
+export const DescriptionHeader = styled.h1`
+  font-size: 96px;
+  font-family: "FHWA", sans-serif;
+  font-weight: 400px;
+  letter-spacing: 3px;
+`;
 
-export const DescriptionSubHeader = styled.p``;
+export const DescriptionSubHeader = styled.p`
+  font-size: 40px;
+  font-family: calibri;
+  margin-top: -45px;
+  margin-left: 420px;
+`;
 
-export const DescriptionText = styled.p``;
+export const DescriptionText = styled.p`
+  font-size: 24px;
+  max-width: 620px;
+  font-family: "NexaRegular", sans-serif;
+  line-height: 35px;
+  margin-top: -10px;
+  word-spacing: 10px;
+`;
